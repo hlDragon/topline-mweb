348fba43f67fbb81e84afd98c740ce68d4b6e69e
fishlay <897635602@qq.com>
2019-08-25 15:22:42 +0800

    tabbar组件创建

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import login from '@/views/login'
+import tabbar from '@/views/tabbar'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       name: 'login',
       path: '/login',
       component: login
+    },
+    {
+      path: '/',
+      component: tabbar,
+      children: []
     }
   ]
 })
